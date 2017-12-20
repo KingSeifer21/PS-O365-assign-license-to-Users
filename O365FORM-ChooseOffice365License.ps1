1b5328c0381d809d4ec3ce559c3af432e715f733
KingSeifer21 <34708640+KingSeifer21@users.noreply.github.com>
2017-12-20 10:51:29 +0100

    Update O365FORM-ChooseOffice365License.ps1

--- O365FORM-ChooseOffice365License.ps1
+++ O365FORM-ChooseOffice365License.ps1
@@ -1,6 +1,6 @@
 ﻿#This script assigns a Office 365 License to Users.This can be new users but also existing users.
 #Fill in a Global Admin username and Password (this can be your own Office 365 account if you are a global admin in the Office 365 tenant)
-#Scripts asks for the UserPrincipleName = Amanuyl Ok = aok@deanone.nl
+#Scripts asks for the UserPrincipleName
 #Afterwards choose the subscription you want to assign, commonly option A is the most used option
 #Exit Script with X and the license will be assigned to the user, to check execute the followin Powershell command:
 #Get-MsolUser -UserPrincipalName "Username" value under license must be set on TRUE. This way you know that the script worked..or check the Office 365 Portal
@@ -11,7 +11,7 @@ $Usercredential = Get-Credential
 Connect-MsolService -AzureEnvironment AzureCloud -Credential $Usercredential
 $UserInOffice365 = READ-HOST -PROMPT 'Type in UserPrinciple Name'
 Get-MsolUser -UserPrincipalName $UserInOffice365
-Set-MsolUser -UserPrincipalName $UserInOffice365 -UsageLocation NL
+Set-MsolUser -UserPrincipalName $UserInOffice365 -UsageLocation "TYPE IN YOUR COUNTRYCODE"
 
 do {
     do {
@@ -38,25 +38,25 @@ do {
         "A"
         {
             write-host "You entered 'A'"
-            Set-MsolUserLicense -UserPrincipalName $UserInOffice365 -addLicense "onexs:O365_BUSINESS_PREMIUM"
+            Set-MsolUserLicense -UserPrincipalName $UserInOffice365 -addLicense "TENANTNAME:O365_BUSINESS_PREMIUM"
         }
         
         "B"
         {
             write-host "You entered 'B'"
-            Set-MsolUserLicense -UserPrincipalName $UserInOffice365 -addLicense "onexs:ENTERPRISEPACK"
+            Set-MsolUserLicense -UserPrincipalName $UserInOffice365 -addLicense "TENANTNAME:ENTERPRISEPACK"
         }
 
         "C"
         {
             write-host "You entered 'C'"
-            Set-MsolUserLicense -UserPrincipalName $UserInOffice365 -addLicense "onexs:VISIOCLIENT"
+            Set-MsolUserLicense -UserPrincipalName $UserInOffice365 -addLicense "TENANTNAME:VISIOCLIENT"
         }
 
         "D"
         {
             write-host "You entered 'D'"
-            Set-MsolUserLicense -UserPrincipalName $UserInOffice365 -addLicense "onexs:EXCHANGESTANDARD"
+            Set-MsolUserLicense -UserPrincipalName $UserInOffice365 -addLicense "TENANTNAME:EXCHANGESTANDARD"
         }
     }
 } until ( $choice -match "X" )
